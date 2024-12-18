3132e58bb0d659650d927d9cc0d75f09372513bc
marziyeh.karani <marziyeh.karani@engitel.com>
2024-12-18 14:07:02 +0100

    fixed slider issue && video message issue

--- src/components/Common/NumericSlider.js
+++ src/components/Common/NumericSlider.js
@@ -1,42 +1,63 @@
-import React from 'react';
-import Slider from 'react-rangeslider';
-import "react-rangeslider/lib/index.css";
-import { useState, useEffect } from 'react';
-
+import React from "react"
+import Slider from "react-rangeslider"
+import "react-rangeslider/lib/index.css"
+import { useState, useEffect,useRef } from "react"
 
 const NumericSlider = ({ min, max, minLabel, maxLabel, onValueChange }) => {
+  var labels = []
+  const [value, setValue] = useState()
+  const sliderRef = useRef(null)
 
-    var labels = {};
-    const [value, setValue] = useState();
+  function handleValueChange(value) {
+    setValue(value)
+    onValueChange(value)
+  }
 
-    function handleValueChange(value) {
-        setValue(value);
-        onValueChange(value)
-    }
+  for (let index = min; index <= max; index++) {
+    labels[index] = index
+  }
 
-    for (let index = min; index <= max; index++) {
-        labels[index] = index;
-    }
+  const getLabelPosition = () => {
+    if (!sliderRef.current) return 0
+    const sliderWidth = sliderRef.current.offsetWidth
+    const valuePercent = (value / 100) * 100 // Convert value to percentage (assuming max is 100)
+    return (sliderWidth * valuePercent) / 100
+  }
 
-    return (
+  useEffect(() => {
+    // Recalculate the label position whenever the slider value changes
+  }, [value])
 
-        <div>
-            <div className="d-flex justify-content-between">
-                <strong>{minLabel}</strong>
-                <strong>{maxLabel}</strong>
-            </div>
-            <Slider
-                value={value}
-                min={min}
-                max={max}
-                labels={labels}
-                orientation="horizontal"
-                onChange={value => {
-                    handleValueChange(value);
-                }}
-            />
-        </div>
-    );
+  return (
+    <div ref={sliderRef}>
+      <div className="d-flex justify-content-between">
+        <strong>{minLabel}</strong>
+
+        <strong>{maxLabel}</strong>
+      </div>
+      <Slider
+        value={value}
+        min={min}
+        max={max}
+        // labels={labels}
+        orientation="horizontal"
+        onChange={value => {
+          handleValueChange(value)
+        }}
+      />
+      <div
+        style={{
+          position: "absolute",
+          left: `${getLabelPosition()}px`,
+          top: "65px", // Adjust the distance between the label and the slider
+          transform: "translateX(-50%)",
+          fontSize: "14px",
+        }}
+      >
+        {value}
+      </div>
+    </div>
+  )
 }
 
-export default NumericSlider;
+export default NumericSlider
